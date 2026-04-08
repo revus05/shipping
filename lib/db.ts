1a1ae3b1ff3a06e9868a28dd3eac1224e159156d
revus05 <skninni05@gmail.com>
2026-04-08 16:27:24 +0300

    feat: update start command

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@/generated/prisma/client"
 
 function normalizePostgresUrl(connectionString: string) {
   if (
